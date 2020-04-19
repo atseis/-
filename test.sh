dbb0735a90414fd9c2cd611e97bfe1b94c06ee73
WY <tourist327@gmail.com>
2020-04-19 10:25:51 +0800

    备案管理系统1.0

--- test.sh
+++ test.sh
@@ -1,3 +1,35 @@
 echo a >> ttt
 git add .
-git commit -m "需求完善"
+git commit -m "备案管理系统1.0"
+
+echo a >> ttt
+git add .
+git commit -m "配置管理系统1.0"
+
+echo a >> ttt
+git add .
+git commit -m "报表管理系统1.0、数据管理系统1.0"
+
+echo a >> ttt
+git add .
+git commit -m "数据分析系统1.0"
+
+echo a >> ttt
+git add .
+git commit -m "系统管理"
+
+echo a >> ttt
+git add .
+git commit -m "通知系统1.0、安全管理系统1.0"
+
+echo a >> ttt
+git add .
+git commit -m "调整数据管理系统对接bug001"
+
+echo a >> ttt
+git add .
+git commit -m "安全管理系统2.0——加入补丁"
+
+echo a >> ttt
+git add .
+git commit -m "用户说明书"
